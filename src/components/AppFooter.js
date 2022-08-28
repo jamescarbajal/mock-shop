09c7a5a7759c68dd5c737c64d6d91525976edda9
James C <you@example.com>
2022-08-27 17:15:25 -0700

    add: footer content and proto links

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -4,9 +4,37 @@ export default function AppFooter() {
 
     return (
         <FooterContainer>
-            <div class="site-footer">
-                This is the footer!
-            </div>
+                <footer class="site-footer">
+                    <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-9 px-5">
+                            <h6 style={{ fontSize: 20, marginLeft: 20 }}>ABOUT</h6>
+                            <p>Brother Ryan Shop is... </p>
+                        </div>
+
+                        <div class="col-12 col-md-3 px-5">
+                            <h6>Quick Links</h6>
+                            <ul class="footer-links">
+                                <li><a href="./Order.html" target="_blank">ORDER</a></li>
+                                <li><a href="./FAQ.html" target="_blank">FAQ</a></li>
+                                <li><a href="./Menu.html" target="_blank">MENU</a></li>
+                                <li><a href="./Contact.html" target="_blank">CONTACT</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    </div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="d-flex flex-row justify-content-center align-items-center">
+                            <p class="copyright-text text-center">
+                                Copyright &copy; 2022 All Rights Reserved by <i>All Things Sweet</i>
+                            </p>
+                            </div>
+
+                        </div>
+                    </div>
+                </footer>
         </FooterContainer>
     );
 };
