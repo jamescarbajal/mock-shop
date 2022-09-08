289649b8c4762780dd0e107c669b167367d9d4c5
James C <you@example.com>
2022-09-08 10:19:08 -0700

    remove: unlinked cart context items and will redo with Redux after making all components

--- src/components/CartModal.js
+++ src/components/CartModal.js
@@ -1,33 +1,18 @@
 import ReactModal from 'react-modal';
 import { CartModalWrapper } from './StyledComponents';
 import { useContext } from 'react';
-import CartContext from '../contexts/Cart/CartContext';
+
 
 
 export default function CartModal(props) {
 
     const { isCartOpen } = props;
 
-    const { cartItems } = useContext(CartContext);
-
     return (
 
         <ReactModal style={{ overlay: { top: '45px', left: '', border: 'none', height: '500px', width: '325px', background: 'none', zIndex: '1' } }} isOpen={isCartOpen}>
             <CartModalWrapper>
-                <div>
-                {cartItems.length > 0 ? (
-                  cartItems.map( (product) => (
-                    <div product={product} id={product.id} title={product.title} price={product.price} category={product.category} description={product.description} image={product.image} />
-                  ))
-
-                )
-                :
-                (
-                  <div>
-                    Cart is empty!
-                  </div>
-                )}
-              </div>
+                Cart is empty
             </CartModalWrapper>
         </ReactModal>
 
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,14 +1,11 @@
 import { CardWrapper, CardHeader, ProductImage, CardButton } from "./StyledComponents";
 import { useState, useContext } from 'react';
-import CartContext from "../contexts/Cart/CartContext";
 import LazyLoad from 'react-lazy-load';
 import ProductModal from './ProductModal';
 
 export default function ProductCard(props) {
 
     const { product, category, description, id, image, price, rating, count, title } = props;
-
-    const { addToCart, increase, cartItems, sumItems, itemCount } = useContext(CartContext);
 
     const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -27,12 +24,13 @@ export default function ProductCard(props) {
                     <CardHeader>{title}</CardHeader>
                 </div>
                 <div class="d-flex flex-row justify-content-between w-100 mt-3">
-                    <CardButton onClick={ViewProductClick}>VIEW</CardButton>
+                    <CardButton onClick={ViewProductClick}>
+                        VIEW
+                    </CardButton>
                     
-                    <CardButton onClick={() => addToCart(product)}>
+                    <CardButton>
                         ADD TO CART
                     </CardButton>
-
                 </div>
             </CardWrapper>
 
