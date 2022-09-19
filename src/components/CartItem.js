55952a7c7b35884b092355a0d5cbb09eb5335413
James C <you@example.com>
2022-09-19 11:46:00 -0700

    add: cart item sub totals with toFixed(2)

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -2,6 +2,7 @@ import { ProductImage, CartHeader, CartItemContainer, CartDescription } from "./
 import { useState } from "react";
 import TruncateMarkup from "react-truncate-markup";
 import ProductModal from "./ProductModal";
+import Cart from "./Cart";
 
 export default function CartItem(props) {
 
@@ -13,6 +14,8 @@ export default function CartItem(props) {
 
     const { id, title, description, category, price, image, quantity } = props;
 
+    const itemTotal = (price * quantity).toFixed(2);
+
     const ViewProductClick = (e) => {
         e.preventDefault();
         setIsModalOpen(true);
@@ -21,18 +24,23 @@ export default function CartItem(props) {
     return(
     <>
             <CartItemContainer>
-            <div class="row border border-solid rounded w-100">
-                <div class="col-12 col-md-6 d-flex flex-column align-items-center" onClick={ViewProductClick} style={{ cursor: 'pointer' }}>
+            <div class="row border border-solid rounded w-100 p-2">
+                <div class="col-12 col-md-6 d-flex flex-column align-items-center" onClick={ViewProductClick} style={{ cursor: 'pointer', boxShadow: '3px 3px 2px lightgray' }}>
                     <ProductImage style={{ margin: '10px', padding: '5px' }} src={image} alt={title}></ProductImage>
                     <CartHeader style={{ fontSize: '18px'}}>{title}</CartHeader>
                     <TruncateMarkup lines={3}>
-                        <div style={{ width: '400px', marginBottom: '30px' }}>{description}</div>
-
+                        <div style={{ maxWidth: '400px', marginBottom: '30px' }}>{description}</div>
                     </TruncateMarkup>
                 </div>
-                <div class="col-12 col-md-6" style={{ minHeight:'200px', display: 'flex', flexDirection: 'column', justifyContent: 'space-around' }}>
-                    <CartHeader>${price}</CartHeader>
-                    <CartHeader>Qty: {quantity}</CartHeader>
+                <div class="row col-12 col-md-6">
+                    <div class="col-12 col-md-6" style={{ minHeight:'200px', display: 'flex', flexDirection: 'column', justifyContent: 'space-around' }}>
+                        <CartHeader>Price: ${price}</CartHeader>
+                        <CartHeader>Qty: {quantity}</CartHeader>
+                        <CartHeader>Subtotal: ${itemTotal}</CartHeader>
+                    </div>
+                    <div class="col-12 col-md-6" style={{ minHeight:'200px', display: 'flex', flexDirection: 'column', justifyContent: 'space-around' }}>
+                        <CartHeader></CartHeader>
+                    </div>
                 </div>
             </div>
 
