efc266e08695e34a0b15a650f30df4f271fb74fa
James C <you@example.com>
2022-09-19 11:27:52 -0700

    add: truncate and modal to cart items

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,20 +1,34 @@
-import { ProductImage, CartHeader, CartItemContainer, CartDetail, CartDescription } from "./StyledComponents";
-import { useState, useEffect } from "react";
+import { ProductImage, CartHeader, CartItemContainer, CartDescription } from "./StyledComponents";
+import { useState } from "react";
+import TruncateMarkup from "react-truncate-markup";
+import ProductModal from "./ProductModal";
 
 export default function CartItem(props) {
 
     const [ currentItem, setCurrentItem ] = useState();
 
+    const [ isModalOpen, setIsModalOpen ] = useState(false);
+
+    const [ isModalLoading, setIsModalLoading ] = useState(false);
+
     const { id, title, description, category, price, image, quantity } = props;
+
+    const ViewProductClick = (e) => {
+        e.preventDefault();
+        setIsModalOpen(true);
+    };
 
     return(
     <>
             <CartItemContainer>
             <div class="row border border-solid rounded w-100">
-                <div class="col-12 col-md-6 d-flex flex-column align-items-center">
+                <div class="col-12 col-md-6 d-flex flex-column align-items-center" onClick={ViewProductClick} style={{ cursor: 'pointer' }}>
                     <ProductImage style={{ margin: '10px', padding: '5px' }} src={image} alt={title}></ProductImage>
                     <CartHeader style={{ fontSize: '18px'}}>{title}</CartHeader>
-                    <CartDescription>{description}</CartDescription>
+                    <TruncateMarkup lines={3}>
+                        <div style={{ width: '400px', marginBottom: '30px' }}>{description}</div>
+
+                    </TruncateMarkup>
                 </div>
                 <div class="col-12 col-md-6" style={{ minHeight:'200px', display: 'flex', flexDirection: 'column', justifyContent: 'space-around' }}>
                     <CartHeader>${price}</CartHeader>
@@ -26,6 +40,8 @@ export default function CartItem(props) {
         <div class="">
 
         </div>
+
+        <ProductModal category={category} description={description} id={id} image={image} price={price} title={title} isModalOpen={isModalOpen} quantity={quantity} setIsModalOpen={setIsModalOpen} />
     </>
     );
 };
